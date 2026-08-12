ff0fe5b115a60e5d45909286a7ec2d3b0f48f143
achavalfelix-crypto <achavalfelix@gmail.com>
2026-08-12 09:00:45 -0300

    Java trbajo

---
+++ live-coding-inicio/script.js
@@ -0,0 +1,16 @@
+console.log("Bienvenidos a la primera actividad");
+console.log("Ahora vamos a comenzar con la primera actividad");
+
+const botonSaludo = document.querySelector("#boton-saludo");
+const mensajeSaludo = document-querySelector("#mensaje");
+
+const miNombre = "Felix";
+const miEdad = 19;
+
+console.log("Resultado de la busqueda:");
+console.log(mensajeSaludo);
+
+botonSaludo.addEventListener("click", function () {
+    console.log("Apretaste el boton");
+    mensajeSaludo.innerText = "Hola"+ miNombre;
+});
