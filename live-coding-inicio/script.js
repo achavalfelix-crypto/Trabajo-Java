d9e62642b208a2877d39332c41a1f90e64f2b96c
achavalfelix-crypto <achavalfelix@gmail.com>
2026-08-18 08:43:32 -0300

    tema oscuro

--- live-coding-inicio/script.js
+++ live-coding-inicio/script.js
@@ -22,5 +22,5 @@ botonSaludo.addEventListener("click", function () {
 
 botonTema.addEventListener("click", function () {
     console.log("Apretaste el boton tema");
-    console.log(document.body.classList.add());
+    console.log(document.body.classList.add("styles.css"));
 })
