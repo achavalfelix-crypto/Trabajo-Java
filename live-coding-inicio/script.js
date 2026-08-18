478393d07fca99a132f166c078c38d42e791bf28
achavalfelix-crypto <achavalfelix@gmail.com>
2026-08-18 08:26:52 -0300

    Boton tema

--- live-coding-inicio/script.js
+++ live-coding-inicio/script.js
@@ -1,16 +1,25 @@
 console.log("Bienvenidos a la primera actividad");
 console.log("Ahora vamos a comenzar con la primera actividad");
 
+
 const botonSaludo = document.querySelector("#boton-saludo");
-const mensajeSaludo = document-querySelector("#mensaje");
+const mensajeSaludo = document.querySelector("#mensaje");
+const botonTema = document.querySelecetor("boton-tema");
+
 
 const miNombre = "Felix";
 const miEdad = 19;
 
+
 console.log("Resultado de la busqueda:");
 console.log(mensajeSaludo);
+
 
 botonSaludo.addEventListener("click", function () {
     console.log("Apretaste el boton");
     mensajeSaludo.innerText = "Hola"+ miNombre;
 });
+
+botonTema.addEventListener("click", function () {
+    console.log("Apretaste el boton tema");
+})
